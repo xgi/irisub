6da4f6463e42483c6aac2536633736aaa276f620
Jake Robertson <jake@faltro.com>
2022-02-18 15:55:50 -0800

    "select video file" -> "select video"

--- components/FileDrop.tsx
+++ components/FileDrop.tsx
@@ -63,7 +63,7 @@ const FileDrop: React.FC<Props> = (props: Props) => {
               <path d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z" />
             </svg>
           </span>
-          <span>Select video file</span>
+          <span>Select video</span>
         </div>
         <input
           className={styles.picker}
