6a6c56ce63caebc35c6ca36152930c8f9196047e
Jake Robertson <jake@faltro.com>
2022-12-20 19:52:24 -0800

    support editing project title

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,9 +1,52 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { currentProjectState } from "../store/states";
 import styles from "../styles/components/Header.module.scss";
 
 type Props = {};
 
 const Header: React.FC<Props> = (props: Props) => {
+  const [currentProject, setCurrentProject] = useRecoilState(currentProjectState);
+  const [editingProjectTitle, setEditingProjectTitle] = useState(false);
+  const [tempProjectTitle, setTempProjectTitle] = useState("");
+
+  const updateProjectTitle = () => {
+    setEditingProjectTitle(false);
+    if (currentProject) {
+      setCurrentProject({ ...currentProject, title: tempProjectTitle });
+    }
+  };
+
+  useEffect(() => {
+    if (editingProjectTitle && currentProject) setTempProjectTitle(currentProject.title || "");
+  }, [editingProjectTitle]);
+
+  const renderProjectTitle = () => {
+    if (!currentProject) return;
+
+    if (editingProjectTitle) {
+      return (
+        <form onSubmit={() => updateProjectTitle()} className={styles.project}>
+          <input
+            value={tempProjectTitle}
+            placeholder="Untitled project"
+            onChange={(e) => setTempProjectTitle(e.target.value)}
+            onFocus={(e) => e.target.select()}
+            onBlur={() => updateProjectTitle()}
+            onSubmit={(e) => console.log("here")}
+            autoFocus
+          />
+        </form>
+      );
+    } else {
+      return (
+        <span onClick={() => setEditingProjectTitle(true)} className={styles.project}>
+          {currentProject?.title || "Untitled project"}
+        </span>
+      );
+    }
+  };
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
@@ -11,6 +54,7 @@ const Header: React.FC<Props> = (props: Props) => {
           <a href="/" className={styles.brand}>
             <span>IRISUB</span>
           </a>
+          {renderProjectTitle()}
         </div>
         <div className={styles.group}>top right</div>
       </header>
