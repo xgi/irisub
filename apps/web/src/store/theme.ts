c0601c119659d808148705e145cf34a7f9d2b9d0
Jake Robertson <jake@faltro.com>
2023-01-03 16:25:48 -0800

    add custom theme states

---
+++ apps/web/src/store/theme.ts
@@ -0,0 +1,39 @@
+import { atom } from "recoil";
+import { localStorageEffect } from "./effects";
+import storeKeys from "../constants/storeKeys.json";
+
+const THEMES = <const>["system", "light", "dark"];
+const ACCENTS = <const>[
+  "green",
+  "teal",
+  "blue",
+  "indigo",
+  "purple",
+  "yellow",
+  "orange",
+  "red",
+  "pink",
+];
+
+type Theme = typeof THEMES[number];
+type Accent = typeof ACCENTS[number];
+
+export const nextTheme = (theme: Theme) => {
+  return THEMES[(THEMES.indexOf(theme) + 1) % THEMES.length];
+};
+
+export const nextAccent = (accent: Accent) => {
+  return ACCENTS[(ACCENTS.indexOf(accent) + 1) % ACCENTS.length];
+};
+
+export const themeState = atom<typeof THEMES[number]>({
+  key: "themeState",
+  default: "system",
+  effects: [localStorageEffect(storeKeys.WORKSPACE.THEME)],
+});
+
+export const accentState = atom<typeof ACCENTS[number]>({
+  key: "accentState",
+  default: "indigo",
+  effects: [localStorageEffect(storeKeys.WORKSPACE.ACCENT)],
+});
