438701e2eec15d1100ab50ee3626f83fc376850c
Jake Robertson <jake@faltro.com>
2023-01-12 19:30:00 -0800

    update shared eslint settings

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,12 @@
 module.exports = {
-  extends: [, "turbo", "prettier"],
+  extends: ["turbo", "react-app", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
   rules: {
     "react/jsx-key": "off",
   },
