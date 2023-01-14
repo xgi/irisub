262276b97451a15c51f545b72f4d89e6df66d0a0
Jake Robertson <jake@faltro.com>
2023-01-13 20:22:14 -0800

    cleanup portal definition

--- packages/irisub-web/src/components/Portal.tsx
+++ packages/irisub-web/src/components/Portal.tsx
@@ -18,8 +18,6 @@ function Portal({ children, wrapperId = "portal-root" }: Props) {
   useLayoutEffect(() => {
     let element = document.getElementById(wrapperId);
     let systemCreated = false;
-    // if element is not found with wrapperId or wrapperId is not provided,
-    // create and append to body
     if (!element) {
       systemCreated = true;
       element = createWrapperAndAppendToBody(wrapperId);
@@ -27,16 +25,13 @@ function Portal({ children, wrapperId = "portal-root" }: Props) {
     setWrapperElement(element);
 
     return () => {
-      // delete the programatically created element
       if (systemCreated && element && element.parentNode) {
         element.parentNode.removeChild(element);
       }
     };
   }, [wrapperId]);
 
-  // wrapperElement state will be null on very first render.
   if (wrapperElement === null) return null;
-
   return createPortal(children, wrapperElement);
 }
 
