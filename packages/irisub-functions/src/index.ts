1b4c44dcd0cb51722374058a60a1ec60c4a6b951
Jake Robertson <jake@faltro.com>
2023-01-16 04:23:42 -0800

    add signup processor function

--- packages/irisub-functions/src/index.ts
+++ packages/irisub-functions/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import * as functions from "firebase-functions";
+import admin from "firebase-admin";
 import { appleFunc } from "irisub-common/apple";
+
+admin.initializeApp(functions.config().firebase);
 
 const app = express();
 
@@ -16,3 +19,21 @@ app.get("*", (req, res) => {
 });
 
 export const server = functions.https.onRequest(app);
+
+export const processSignUp = functions.auth.user().onCreate((user) => {
+  console.log(user);
+  const customClaims = {
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-default-role": "user",
+      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-user-id": user.uid,
+    },
+  };
+
+  return admin
+    .auth()
+    .setCustomUserClaims(user.uid, customClaims)
+    .catch((error) => {
+      console.log(error);
+    });
+});
